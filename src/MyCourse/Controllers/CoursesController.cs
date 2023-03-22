b0b763dc96828f5f00fbd7e2853cbfda9a44e6f5
davideTwin <oreglio@twin-online.it>
2023-03-22 16:33:21 +0100

    miglioro rotta middleware

--- src/MyCourse/Controllers/CoursesController.cs
+++ src/MyCourse/Controllers/CoursesController.cs
@@ -6,8 +6,16 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace MyCourse.Controllers
 {
-    public class CoursesController : Controller 
+    public class CoursesController : Controller
     {
-        
+        public IActionResult index()
+        {
+            return Content("sono index");
+        }
+
+        public IActionResult Detail(string id)
+        {
+            return Content($"sono detail il mio id è {id}");
+        }
     }
 }
--- src/MyCourse/Startup.cs
+++ src/MyCourse/Startup.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace MyCourse
@@ -15,6 +16,7 @@ namespace MyCourse
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -27,10 +29,10 @@ namespace MyCourse
             }
             app.UseStaticFiles();
             
-            app.Run(async (context) =>
+            //app.UseMvcWithDefaultRoute();
+            app.UseMvc(routeBuilder =>
             {
-                string nome = context.Request.Query["nome"];
-                await context.Response.WriteAsync($"Hello {nome}!");
+                routeBuilder.MapRoute("default","{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
