052856dce9707c7eb181df7d21071b8524df3587
davideTwin <oreglio@twin-online.it>
2023-03-23 10:56:11 +0100

    add bs & fontawesome header&footer

--- src/MyCourse/Startup.cs
+++ src/MyCourse/Startup.cs
@@ -10,34 +10,33 @@ using Microsoft.Extensions.DependencyInjection;
 
 namespace MyCourse
 {
-    public class Startup
-    {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddMvc();
-        }
+   public class Startup
+   {
+      // This method gets called by the runtime. Use this method to add services to the container.
+      // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+      public void ConfigureServices(IServiceCollection services)
+      {
+         services.AddMvc();
+      }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
-        {
-            //if (env.IsDevelopment())
-            if (env.IsEnvironment("Development"))
-            {
-                app.UseDeveloperExceptionPage();
-            }
-            app.UseStaticFiles();
-            
-            app.UseMvc(routeBuilder =>
-            //può contenere più rotte semplicemente usa il primo conforme che trova 
-            {
-                //per esempio /courses/detail/69 oppure in caso di default: Home/Index/id(opzionale)
-                routeBuilder.MapRoute("default","{controller=Home}/{action=Index}/{id?}");
+      // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+      public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+      {
+         //if (env.IsDevelopment())
+         if (env.IsEnvironment("Development"))
+         {
+            app.UseDeveloperExceptionPage();
+         }
+         app.UseStaticFiles();
 
-                routeBuilder.MapRoute("search","{controller}/{action=Search}/{title}");
+         app.UseMvc(routeBuilder =>
+         //può contenere più rotte semplicemente usa il primo conforme che trova 
+         {
+            //per esempio /courses/detail/69 oppure in caso di default: Home/Index/id(opzionale)
+            routeBuilder.MapRoute("default", "{controller=Home}/{action=Index}/{id?}");
 
-            });
-        }
-    }
+            routeBuilder.MapRoute("search", "{controller}/{action=Search}/{title}");
+         });
+      }
+   }
 }
